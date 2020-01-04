5bcae64929727f6d5814ec677eb411847ce176f9
ElPapuso <miltonnavarro023@gmail.com>
2020-01-03 20:06:04 -0500

    chat

--- src/app/pages/conductores/conductores.page.ts
+++ src/app/pages/conductores/conductores.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { MainService } from "../../servicios/main.service";
+import { AuthService } from 'src/app/servicios/auth.service';
+import { UserInterface } from '../../../app/models/user';
+import { MenuController } from '@ionic/angular';
+import { GuiasService } from "../../servicios/guias.service";
 
 @Component({
   selector: 'app-conductores',
@@ -6,10 +13,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conductores.page.scss'],
 })
 export class ConductoresPage implements OnInit {
-
-  constructor() { }
+  array: any;
+  array2: any;
+  constructor(
+    private guiaService: GuiasService,
+    private loadingController: LoadingController,
+    private mainService: MainService,
+    private Router: Router,
+    private toastController: ToastController,
+    private authService: AuthService,
+    private menu: MenuController
+  ) { }
 
   ngOnInit() {
+    this.guiaService.getAllGuias().subscribe(res => this.array2 = res);
+    this.mainService.getAllEventos().subscribe(res => {
+      this.array = res;
+      console.log(this.array);
+    });
+
+    this.authService.isAuth().subscribe(user => {
+      if (user) {
+        console.log(user);
+        this.user.name = user.displayName;
+        this.user.email = user.email;
+        this.user.photoUrl = user.photoURL;
+      }
+    });
   }
+
+  openCustom() {
+    this.menu.enable(true, 'cn');
+    this.menu.open('cn');
+  }
+  atras() {
+    this.menu.close('cn');
+  }
+
+  user: UserInterface = {
+    name: '',
+    email: '',
+    photoUrl: '',
+  };
+
+  public providerId: string = 'null';
 
 }
